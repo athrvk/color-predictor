5ab5ff87996c71f0d180f3367e43e2c60a7fabaa
Atharva <atharvakusumbia@gmail.com>
2020-10-14 19:54:49 +0530

    Updates

---
+++ precache-manifest.d9876aff44214c8608cafebddabad35b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4669b868e1457c9ec31a820c9099c0",
+    "url": "/color-predictor/index.html"
+  },
+  {
+    "revision": "f648194c55f67aac8ab9",
+    "url": "/color-predictor/static/css/main.9ebaf2bf.chunk.css"
+  },
+  {
+    "revision": "dbcd34f969f4c4ece5b7",
+    "url": "/color-predictor/static/js/2.de2e47e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-predictor/static/js/2.de2e47e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f648194c55f67aac8ab9",
+    "url": "/color-predictor/static/js/main.26679e7c.chunk.js"
+  },
+  {
+    "revision": "2d8fff99e850ceece9e917d652805b60",
+    "url": "/color-predictor/static/js/main.26679e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a029b88b3b4d578ed2dd",
+    "url": "/color-predictor/static/js/runtime-main.77ecc5b2.js"
+  }
+]);
